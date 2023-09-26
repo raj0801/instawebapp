13316da90e93962d3bc58bc08d35f98ed5d9d7ad
raj0801 <pr1650644@gmail.com>
2023-09-26 10:46:52 +0530

    commint title

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
